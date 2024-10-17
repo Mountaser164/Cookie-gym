bd332d4e8e6575251228e131e7b82062e850bc62
Mountaser164 <9015678@student.zadkine.nl>
2024-10-17 13:57:13 +0200

    donderdag2

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 let score = 0;
 let AutoClickers = 0;
 let Upgrad = 0;
-let second = 5000;
+let second = 3000;
 let gosetInterval = 0;
+let upgradseninterval = 0;
+let second2 = 2000;
+
 
 
 
@@ -39,7 +42,7 @@ function buy(){
     }
         
 }
-
+ 
 function showScore(){
 
     document.getElementById("scoor").innerHTML=score;
@@ -52,12 +55,16 @@ function Upgrades(){
     if(score >= 20){
         score = score -20;
         Upgrad++;
+        showhand2()
         showScore();
+        
 
         document.getElementById("overzicht2").innerHTML=Upgrad;
         document.getElementById("showupgrades").innerHTML= Upgrad; 
 
         alert("Your buy is successfull");
+        
+        startUpgradcliker()
 
     }else{
         alert("You don't have enough cookies");
@@ -74,7 +81,6 @@ function startOutocliker(){
     if( gosetInterval == 0)
         gosetInterval = setInterval(handenaddpoint, second);
 
- 
 }
 
 function showhand(){
@@ -83,5 +89,24 @@ document.getElementById("handen").innerHTML +=' <img class = "hand"src="icons8-h
 
 function handenaddpoint(){
     score = score + AutoClickers;
+    score = score + Upgrad;
     showScore();
 }
+
+
+
+
+
+function startUpgradcliker(){
+
+    if(upgradseninterval == 0)
+        upgradseninterval = setInterval(handenaddpoint, second2);
+        
+
+}
+
+
+function showhand2(){
+    document.getElementById("handen2").innerHTML +=' <img class = "hand"src="icons8-hand-24.png" alt="handfoto">'
+    }
+
